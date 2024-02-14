edb0c7dd9ce033959890317c6cd6b2dc8bbfd1db
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-02-14 21:13:43 +0800

    added a function

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 let userScore = 0;
 let computerScore = 0;
+
+function makeComputerChoice(){
+    const choices = [1,2,3,4,5,6];
+    const choice = choices[Math.floor(Math.random() * 7)];
+    return(choice);
+
+}
